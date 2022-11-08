4aa5bd136c10feebcba7886d2f8a3a08a39efd59
SanthoshK98 <santhoshkumarkoneti98@gmail.com>
2022-11-08 16:21:10 +0530

    added hello world

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import React,{useState, useEffect}from 'react'
+import React,{useEffect}from 'react'
 import { io } from 'socket.io-client';
 
-const socket = io.connect("http://localhost:5000")
+
 
 function App() {
+  const socket = io.connect("http://localhost:5000")
   const sendMessage = ()=>{
     socket.emit("send_message",{message:"Hello"})
   }
@@ -15,6 +16,7 @@ function App() {
   },[socket])
   return (
     <div className="App">
+      <h1>Hello World</h1>
       <input placeholder='Message......'/>
       <button onClick={sendMessage}>Send</button>
     </div>
