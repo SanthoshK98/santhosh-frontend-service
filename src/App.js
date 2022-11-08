7d8622f88555e1e714d2af5c038ec17ba42e3d29
SanthoshK98 <93116258+SanthoshK98@users.noreply.github.com>
2022-11-08 16:30:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
-import React,{useState, useEffect}from 'react'
-import { io } from 'socket.io-client';
+// import React,{useState, useEffect}from 'react'
+// import { io } from 'socket.io-client';
 
 
 
 function App() {
-  const socket = io.connect("http://localhost:5000")
-  const sendMessage = ()=>{
-    socket.emit("send_message",{message:"Hello"})
-  }
-  useEffect(()=>{
-    socket.on("receive_message",(data)=>{
-      alert(data.message)
-    })
-  },[socket])
+//   const socket = io.connect("http://localhost:5000")
+//   const sendMessage = ()=>{
+//     socket.emit("send_message",{message:"Hello"})
+//   }
+//   useEffect(()=>{
+//     socket.on("receive_message",(data)=>{
+//       alert(data.message)
+//     })
+//   },[socket])
   return (
     <div className="App">
       <h1>Hello React</h1>
       <input placeholder='Message......'/>
-      <button onClick={sendMessage}>Send</button>
+      <button>Send</button>
     </div>
   );
 }
